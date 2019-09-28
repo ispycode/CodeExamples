2b5e5472422fae7f9001b053f7d43d8830246255
Dennis MacConnell <dennis.macconnell@gmail.com>
2019-09-28 14:19:42 -0500

    empty string

---
+++ java-examples/src/main/java/strings/EmptyString.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class EmptyString {
+    public static void main(String[] args) {
+
+        String str = "";
+        if (str != null) {
+            System.out.println(str);
+            System.out.println("Empty: " + str.isEmpty());
+        }
+
+        str = "Hello";
+        if (str != null) {
+            System.out.println(str);
+            System.out.println("Empty: " + str.isEmpty());
+        }
+    }
+}
